cf7228612d080085528b2473713ff7afc60ad4a9
NVORON <nvoron482@gmail.com>
2023-09-26 17:03:51 +0300

    First commit
    Updated images, created player class and gui window

--- main.py
+++ main.py
@@ -1,51 +1,88 @@
-from tkinter import Tk, Listbox, PhotoImage, Button, Frame, Menu
-from pygame import mixer
+from tkinter import Tk, Listbox, PhotoImage, Button, Frame, Menu, Label, Scale, Canvas
+from player import Player
 
 #Окно
 window = Tk()
 window.title("Music Player")
-window.iconbitmap("./gui/logo.ico")
-window.geometry("300x500")
+window.geometry("300x400")
+window.config(bg="#262626")
+window.resizable(False, False)
+window.iconphoto(True, PhotoImage(file=('./gui/logo.png')))
+window.wm_attributes("-transparent", True)
+songs_list = Listbox(master = window, fg = "white", bg = "black", borderwidth = 5, justify = "left", width = 29, font = ('poppins', 14))
 
-#Музыка
-mixer.init()
 
-#Список музыки
-songs_list = Listbox(master = window, width=60)
-songs_list.pack(pady=20)
+#Рамка для врмемени музыки
+time_frame = Frame(window, bg = "#262626")
+start_time = Label(master = time_frame, text='', anchor = "w", bg="#262626", padx = 10)
+end_time = Label(master = time_frame, text='', anchor = "e", bg="#262626", padx = 10)
+music_slider = Scale(master = window, from_ = 0, to = 100, orient="horizontal", bg = "#262626", relief="flat", length = 270, showvalue = False)
 
-#Рамка для кнопок
-btn_frame = Frame(window)
-btn_frame.pack()
+volume_frame = Frame(window, bg = "#262626")
+volume_but = Button(master = volume_frame, borderwidth = 0, relief = "flat", bg = "#262626")
+volume_slider  = Scale(master = volume_frame, from_ = 0, to = 100, relief="flat", orient="horizontal", bg = "#262626", length = 240, showvalue = False, label = "")
+volume_label = Label(master = window, text = '100%', relief="flat", anchor = "center", bg="#262626")
+
+player = Player(songs_list, start_time, end_time, music_slider, volume_label, volume_slider, volume_but)
 
 #Картинки кнопок
-previous_img = PhotoImage(file = "gui/previous.png")
-play_img = PhotoImage(file = "gui/play.png")
-next_img = PhotoImage(file = "gui/next.png")
-pause_img = PhotoImage(file = "gui/pause.png")
+previous_img = PhotoImage(file = "./gui/previous.png")
+play_img = PhotoImage(file = "./gui/play.png")
+pause_img = PhotoImage(file = "./gui/pause.png")
+next_img = PhotoImage(file = "./gui/next.png")
+add_img = PhotoImage(file = "./gui/add.png")
+remove_img = PhotoImage(file = "./gui/remove.png")
 
 #Кнопки
-previous_btn = Button(master = btn_frame, image = previous_img, borderwidth = 0)
-play_btn = Button(master = btn_frame, image = play_img, borderwidth = 0)
-next_btn = Button(master = btn_frame, image = next_img, borderwidth = 0)
-pause_btn = Button(master = btn_frame, image = pause_img, borderwidth = 0)
+btn_frame = Frame(window, bg = "#262626")
+previous_btn = Button(master = btn_frame, image = previous_img, borderwidth = 0, relief = "flat", bg = "#262626", command = player.previous_song)
+play_btn = Button(master = btn_frame, image = play_img, borderwidth = 0, relief = "flat", bg = "#262626", command = player.play_song)
+pause_btn = Button(master = btn_frame, image = pause_img, borderwidth = 0, relief = "flat", bg = "#262626", command = player.pause_song)
+next_btn = Button(master = btn_frame, image = next_img, borderwidth = 0, relief = "flat", bg = "#262626", command = player.next_song)
 
-#Установка кнопок
-previous_btn.grid(row = 0, column = 0)
-play_btn.grid(row = 0, column = 1)
-next_btn.grid(row = 0, column = 2)
-pause_btn.grid(row = 0, column = 3)
+edit_list_frame = Frame(window, bg = "#262626")
+remove_btn = Button(master = edit_list_frame, image = remove_img, borderwidth = 0, relief = "flat", bg = "#262626", command = player.remove)
+add_btn = Button(master = edit_list_frame, image = add_img, borderwidth = 0, relief = "flat", bg = "#262626", command = player.add_file_to_playlist)
+
+songs_list.pack()
+
+edit_list_frame.pack(fill = "x", pady = 5, padx=20)
+remove_btn.pack(side="left", padx=20)
+add_btn.pack(side="right", padx=20)
+
+time_frame.pack(fill = "x", pady = 5, padx = 10)
+start_time.pack(side="left")
+end_time.pack(side="right")
+
+music_slider.pack()
+
+btn_frame.pack(fill = "x", pady = 10)
+previous_btn.grid(row=0, column=0, padx = (12,10))
+play_btn.grid(row=0, column=1, padx = 10)
+pause_btn.grid(row=0, column=2, padx = 10)
+next_btn.grid(row=0, column=3, padx=(10,12))
+
+volume_frame.pack(fill = "x", pady = (5, 0))
+volume_but.pack(padx = (12, 10), side = "left")
+volume_slider.pack(padx = (10, 22), side = "right")
+volume_label.pack()
 
 #Меню
-menu = Menu(window)
-window.config(menu = menu)
+main_menu = Menu(window)
+window.config(menu = main_menu)
 
 #Добавляем в меню возможность добавить музыкальные файлы
-add_song = Menu(menu)
-add_song.add_cascade(Label = "Add Song", menu = add_song)
-
-
+menu = Menu(main_menu)
+main_menu.add_cascade(label = "File", menu = menu)
+menu.add_command(label = "Add Song To Playlist", command = player.add_file_to_playlist)
+menu.add_separator()
+menu.add_command(label = "Clear Playlist", command = player.clear_playlist)
+menu.add_separator()
+# menu.add_command(label = "Quit", command = player.quit)
 
 
 if __name__ == "__main__":
+    volume_but.config(command = player.mute)
+    volume_slider.config(command = player.volume_slide)
+    # window.protocol("WM_DELETE_WINDOW", on_closing)
     window.mainloop()
---
+++ player.py
@@ -0,0 +1,171 @@
+from tkinter import filedialog, messagebox, PhotoImage
+from time import strftime, gmtime, sleep
+from pygame import mixer
+import mutagen
+
+class Player():
+    
+    def __init__(self, songs_list, start_time, end_time, music_slider, volume_label, volume_slider, volume_but):
+        self.mixer = mixer
+        self.songs_list = songs_list
+        self.start_time = start_time
+        self.end_time = end_time
+        self.music_slider = music_slider
+        self.volume_label = volume_label
+        self.volume_slider = volume_slider
+        self.volume_but = volume_but
+        self.songs_list_full = set()
+        self.ispaused = False
+        self.islooped = False
+        self.ismuted = False
+        self.mixer.init()
+        self.volume_slider.set(100)
+        self.mixer.music.set_volume(100)
+        self.volume_none_img = PhotoImage(file = "./gui/volume_none.png")
+        self.volume_low_img = PhotoImage(file = "./gui/volume_low.png")
+        self.volume_mid_img = PhotoImage(file = "./gui/volume_mid.png")
+        self.volume_high_img = PhotoImage(file = "./gui/volume_high.png")
+        self.volume_but.config(image = self.volume_high_img)
+        self.last_volume = 100
+    
+    def get_full_path(self, song_name):
+        for song in self.songs_list_full:
+            if song_name in song:
+                return song
+
+    def add_file_to_playlist(self):
+        songs = filedialog.askopenfilenames(title = "Choose A Song", filetypes=[("mp3 Files", "*.mp3"), 
+                                                                                ("wav Files", "*.wav"),
+                                                                                ("flac Files", "*.flac")]) # Появляется окно для выбора файлов
+        for song in songs: # Перебираем каждую песню из списка
+            if song.split(".")[-1] not in ["mp3", "wav", "flac"]: # На случай, если как-то обошли системную блокировку неподдерживаемых файлов
+                messagebox.showwarning(title = "Неподдерживаемый тип файла", message = "Тип файла нет в списке поддерживаемых файлов!")
+            else:
+                self.songs_list_full.add(song) # Лист с раположениями файлов, чтобы знать, куда обращаться
+                song_name = ".".join([song.split("/")[-1].split(".")[i] for i in range(0, len(song.split("/")[-1].split("."))-1)]) # Вычленяем название файла от папок и типа
+                self.songs_list.insert('end', song_name)
+
+    def remove(self):
+        self.stop()
+        song = self.get_full_path(self.songs_list.get("active"))
+        self.songs_list_full.discard(song)
+        self.songs_list.delete("active")
+
+    def clear_playlist(self):
+        self.stop()
+        self.songs_list_full = set()
+        self.songs_list.delete(0, 'end')
+
+    def stop(self):
+        try:
+            self.start_time.after_cancel(self.loop)
+        except:
+            pass
+        self.mixer.music.stop()
+        self.music_slider.set(0)
+        self.music_slider.config(from_ = 0, to = 100)
+        self.start_time.config(text="")
+        self.end_time.config(text="")
+
+    def previous_song(self):
+        if len(self.songs_list_full) != 0:
+            self.stop()
+            if self.islooped:
+                next_song_id = self.songs_list.curselection()[0]
+            else:
+                try:
+                    next_song_id = self.songs_list.curselection()[0] - 1
+                except:
+                    next_song_id = 0
+
+            self.songs_list.select_clear(0, 'end')
+            self.songs_list.activate(next_song_id)
+            self.songs_list.select_set(next_song_id)
+
+            self.play_song()
+                
+    def play_song(self):
+        if len(self.songs_list_full) != 0:
+            self.stop()
+            song = self.get_full_path(self.songs_list.get("active"))
+            try:
+                self.mixer.music.load(song)
+                self.mixer.music.play(start = 0)
+            except:
+                messagebox.showerror(title = "Ошибка в запуске файла", message = "Не удалось прочитать файл!")
+                self.remove()
+                return
+            self.music_slider.set(0)
+            self.start_time.config(text="00:00:00")
+            self.end_time.config(text = strftime('%H:%M:%S', gmtime(int(mutagen.File(song).info.length))))
+            self.music_slider.config(from_ = 0, to = int(mutagen.File(song).info.length))
+            self.play_time()
+    
+    def pause_song(self):
+        if len(self.songs_list_full) != 0:
+            if self.ispaused:
+                self.mixer.music.unpause()
+            elif not self.ispaused:
+                self.mixer.music.pause()
+            self.ispaused = not self.ispaused
+
+    def next_song(self):
+        if len(self.songs_list_full) != 0:
+            self.stop()
+            if self.islooped:
+                next_song_id = self.songs_list.curselection()[0]
+            else:
+                try:
+                    next_song_id = self.songs_list.curselection()[0] + 1
+                except:
+                    next_song_id = 0
+            self.songs_list.select_clear(0, 'end')
+            self.songs_list.activate(next_song_id)
+            self.songs_list.select_set(next_song_id)
+            self.play_song()
+    
+    def play_time(self):
+        cur_time = int(self.mixer.music.get_pos() / 1000) + 1
+        song = self.get_full_path(self.songs_list.get("anchor"))
+        try:
+            song_info = mutagen.File(song)
+        except:
+            pass
+        if int(self.music_slider.get()) + 1 == song_info.info.length:
+            return self.next_song()
+        elif self.ispaused:
+            pass
+        elif abs(int(self.music_slider.get()) - cur_time) < 2:
+            self.music_slider.set(cur_time)
+            self.start_time.config(text = strftime('%H:%M:%S', gmtime(cur_time-1)))
+        else:
+            self.music_slider.set(int(self.music_slider.get()))
+            self.start_time.config(text = strftime('%H:%M:%S', gmtime(int(self.music_slider.get()))))
+            self.music_slider.set(int(self.music_slider.get()) + 1)
+            try:
+                self.mixer.music.set_pos(int(self.music_slider.get()))
+            except:
+                return self.next_song()
+
+        self.loop = self.start_time.after(1000, self.play_time)
+
+    def volume_slide(self, val):
+        slider_val = int(val)
+        self.mixer.music.set_volume(int(slider_val) / 100.)
+        self.volume_label.config(text = str(slider_val) + "%")
+        if slider_val == 0:
+            self.volume_but.config(image = self.volume_none_img)
+        elif slider_val <= 33:
+            self.volume_but.config(image = self.volume_low_img)
+        elif 33 < slider_val <= 66:
+            self.volume_but.config(image = self.volume_mid_img)
+        elif 66 < slider_val <= 100:
+            self.volume_but.config(image = self.volume_high_img)
+
+    def mute(self):
+        if int(self.volume_slider.get()) == 0:
+            self.volume_slider.set(self.last_volume)
+        else:
+            last_volume_temp = int(self.volume_slider.get())
+            self.volume_slider.set(0)
+            self.last_volume = last_volume_temp
